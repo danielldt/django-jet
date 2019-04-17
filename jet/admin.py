4e4b914cbba5ad9dc1380aba3b3aa0175997f71b
Daniell Toidy <danielltoidy@MacBook-Pro.local>
2019-04-18 01:07:47 +0800

    add default inline

--- jet/admin.py
+++ jet/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 class CompactInline(admin.options.InlineModelAdmin):
     template = 'admin/edit_inline/compact.html'
-class DefaultInline(admin.options.InlineModelAdmin):
-    template = 'admin/edit_inline/default.html'
+class DefaultInline(DjangoUserAdmin):
+    change_form_template = 'admin/edit_inline/default.html'
