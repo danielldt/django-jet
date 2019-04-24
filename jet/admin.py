f628ce065456a20ce7d186e98994988ec5bfea79
Daniell Toidy <danielltoidy@MacBook-Pro.local>
2019-04-24 18:36:08 +0800

    update inline

--- jet/admin.py
+++ jet/admin.py
@@ -3,6 +3,9 @@ from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 class CompactInline(admin.options.InlineModelAdmin):
     template = 'admin/edit_inline/compact.html'
-class DefaultInline(DjangoUserAdmin):
+class DefaultUserInline(DjangoUserAdmin):
+    change_form_template = 'admin/edit_inline/default.html'
+    add_form_template = 'admin/edit_inline/default.html'
+class DefaultInline(admin.ModelAdmin):
     change_form_template = 'admin/edit_inline/default.html'
     add_form_template = 'admin/edit_inline/default.html'
